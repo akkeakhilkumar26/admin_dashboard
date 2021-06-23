2a236e020c59fc198c7bac83bb972e0c7563099e
akhilkumar <akhilkumar.ramphaltech@gmail.com>
2021-06-23 19:24:12 +0530

    made changes in table

--- src/app/home/main/dashboard/data-list/data-list.component.ts
+++ src/app/home/main/dashboard/data-list/data-list.component.ts
@@ -6,20 +6,19 @@ export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
-  symbol: string;
+  action: string;
+  email:string;
+  department:string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'Li'},
+  {position: 2, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'He'},
+  {position: 3, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'H'},
+  {position: 4, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'Be'},
+  {position: 5, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'B'},
+  {position: 6, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'C'},
+  {position: 7, name: 'UserName',email:'abcd@gmail.com',department:'communications',weight: 9876543219, action: 'C'},
 ];
 @Component({
   selector: 'app-data-list',
@@ -27,7 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./data-list.component.css']
 })
 export class DataListComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name','email','department', 'weight', 'action'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   applyFilter(event: Event) {
